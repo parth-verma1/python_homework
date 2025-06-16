c4107248dc3b0139bc96ce47c4914b99f7585e95
parth-verma1 <parth.verma140810@gmail.com>
2025-06-16 17:28:22 +0530

    Add files via upload

---
+++ list-square.py
@@ -0,0 +1,7 @@
+numbers = [2, 4, 6, 1, 9] 
+squared = []
+
+for num in numbers:
+    squared.append(num ** 2)
+
+print("Squared list:", squared)
---
+++ product.py
@@ -0,0 +1,7 @@
+numbers = (2, 3, 5, 1) 
+product = 1
+
+for i in numbers:
+    product *= i
+
+print("Product of all numbers in the tuple is:", product)
