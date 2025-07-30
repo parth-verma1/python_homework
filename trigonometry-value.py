5e3b18c09f76c8ebffde410b3a58775c6d5c2552
parth-verma1 <parth.verma140810@gmail.com>
2025-07-30 17:28:03 +0530

    Add files via upload

---
+++ trigonometry-value.py
@@ -0,0 +1,8 @@
+import math
+
+angle = float(input("enter angle in degrees: "))
+rad = math.radians(angle)
+
+print("sin:", math.sin(rad))
+print("cos:", math.cos(rad))
+print("tan:", math.tan(rad))
